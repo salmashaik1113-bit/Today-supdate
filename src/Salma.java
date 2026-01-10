507934d99fc70dca820a7cc7fbf7171d47bb19b1
salma <salmashaik1113@gmail.com>
2026-01-10 12:34:34 +0530

    Student marks

---
+++ src/Salma.java
@@ -0,0 +1,25 @@
+ class Student {
+     int age;
+     String name;
+     int marks;
+ }
+    public class Salma{
+        public static void main(String[] args) {
+            Student s1=new Student();
+                    s1.age=23;
+            s1.name="shaik";
+            s1.marks=90;
+            Student s2=new Student();
+         s2.age=23;
+         s2.name="salma";
+         s2.marks=90;
+         System.out.println(s1.age);
+         System.out.println(s1.name);
+         System.out.println(s1.marks);
+         System.out.println(s2.age);
+         System.out.println(s2.name);
+         System.out.println(s2.marks);
+
+     }
+     }
+
