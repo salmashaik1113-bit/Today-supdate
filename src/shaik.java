b6a79bb67c1ea2e00293aec5d21374dbfda8f9d4
salma <salmashaik1113@gmail.com>
2026-01-10 12:22:26 +0530

    my first step

---
+++ src/shaik.java
@@ -0,0 +1,12 @@
+public class shaik {
+    public static void main(String[] args){
+        int a=43;
+        char b='s';
+        float c=3;
+        double d=7.8;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
